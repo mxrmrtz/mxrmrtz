916222d04eb9a744873231906639a6919b901103
Maximiliam Reimertz <max.reimertz@gmail.com>
2023-08-10 16:23:28 +0200

    created dynamic information and display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import NavBar from "./components/NavBar";
+import collections from "./Data.json";
 
 import "./App.css";
 import Collection from "./components/Collection";
 
 function App() {
 	return (
-		<main className="main">
-			<NavBar />
-			<Collection />
-		</main>
+	    <main className="main">
+      <NavBar />
+      {collections.collection.map((collection) => (
+        <Collection
+          key={collection.id}
+          name={collection.name}
+          headline={collection.headline}
+          info={collection.info}
+          image={collection.image} // Just pass the image path
+        />
+      ))}
+    </main>
 	);
 }
 
--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,14 +1,12 @@
 import styles from "../styles/collection.module.css";
 import curves from "../images/curves.png";
-const Collection = () => {
+const Collection = ({ name, headline, image, id }) => {
 	return (
 		<main className={styles.container}>
-			<img src={curves} className={styles.img} />
+			<img src={image} className={styles.img} alt={name} />
 			<div className={styles.collection_name}>
-				<h2>curves</h2>
-				<h3>
-					Life is too <b>random</b> to be <b>straight</b>, so it’s okay to <b>curve</b>
-				</h3>
+				<h2>{name}</h2>
+				<h3>{headline}</h3>
 				<button>Discover</button>
 			</div>
 		</main>
